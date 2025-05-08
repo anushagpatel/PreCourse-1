713e58e67ab809cf7fd27193b5e7498d15468ce4
anushagpatel <anushagpatel123@gmail.com>
2025-05-07 22:30:15 -0400

    Done PRECOURSE1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,11 @@
+// Time Complexity:
+// push() - O(1)
+// pop() - O(1)
+// peek() - O(1)
+// isEmpty() - O(1)
+
+// Space Complexity: O(n) where n is MAX (here 1000) for the array storage
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -7,11 +15,13 @@ class Stack {
   
     boolean isEmpty() 
     { 
+        return top < 0;
         //Write your code here 
     } 
 
     Stack() 
     { 
+        top = -1;
         //Initialize your constructor 
     } 
   
@@ -19,22 +29,39 @@ class Stack {
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top >= MAX - 1) {
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[++top] = x;
+            return true;
+        }
     } 
   
+    // Pops the top element from the stack
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            return a[top--];
+        }
     } 
   
+    // Peek the top element.
     int peek() 
     { 
         //Write your code here
+            return a[top];
     } 
 } 
   
 // Driver code 
 class Main { 
+
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
--- Sample.java
+++ Sample.java
@@ -5,3 +5,31 @@
 
 
 // Your code here along with comments explaining your approach
+
+// EXERCISE 1
+// Time Complexity:
+// push() - O(1)
+// pop() - O(1)
+// peek() - O(1)
+// isEmpty() - O(1)
+
+// Space Complexity: O(n) where n is MAX (here 1000) for the array storage
+
+
+//EXERCISE 2
+
+// Time Complexity:
+// push() - O(1)
+// pop() - O(1)
+// peek() - O(1)
+// isEmpty() - O(1)
+
+// Space Complexity: O(n) where n is the number of elements in the stack
+
+//EXERCISE 3
+
+// Time Complexity:
+// insert() - O(n) [because it traverses to the end of the list]
+// printList() - O(n) [it visits each node once]
+
+// Space Complexity: O(n), where n is the number of nodes in the linked list
